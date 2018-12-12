9a3699ce0e4e8837643624d87e055ab51acc7d97
濱田洋輔 <hamada50154@gmail.com>
2018-12-12 22:01:38 +0900

    一覧ページ修正

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,13 +5,16 @@
     @auth
         <a href={{route('articles.create')}} class="btn btn-primary float-right">New Article</a>
     @endauth
+    <div class="container-flex" style="display: flex;flex-wrap: wrap;">
     @foreach ($articles as $article)
-        <article>
-            <h2>
-                <a href="{{url('articles',$article->id)}}">
-                {{$article->title}}
-                </a>
-            </h2>
-        </article>
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">{{$article->title}}</h5>
+        <p class="card-text">{{$article->body}}</p>
+        <a href="{{url('articles',$article->id)}}" class="card-link" style="position: absolute;bottom: 0.6rem;">詳細</a>
+        <a href="#" class="card-link" style="position: absolute;bottom: 0.6rem;margin-left: 3rem;">Another link</a>
+    </div>
+    </div> 
     @endforeach
+    </div>
 @endsection
